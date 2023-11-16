d56cfb1cb44259477d74c03ea17d36029694111c
suvaryanserhiy <suvaryanserhiy.ua@gmail.com>
2023-11-16 14:55:42 +0100

    added some comment to a Database.php

--- models/Database.php
+++ models/Database.php
@@ -1,29 +1,30 @@
 <?php
 class Database {
+    // variable declaration
     private $server;
     private $username;
     private $password;
     private $database;
     private $connection;
 
-    public function __construct($server, $username, $password, $database) {
+    public function __construct($server, $username, $password, $database) { //constructor
         $this->server = $server;
         $this->username = $username;
         $this->password = $password;
         $this->database = $database;
 
-        $this->connect();
+        $this->connect(); //execute connect function
     }
 
     private function connect() {
-        $this->connection = new mysqli($this->server, $this->username, $this->password, $this->database);
+        $this->connection = new mysqli($this->server, $this->username, $this->password, $this->database); // establish connection to a server
 
         if ($this->connection->connect_error) {
             die("Connection failed: " . $this->connection->connect_error);
         }
     }
 
-    public function getConnection() {
+    public function getConnection() { // function to get a connection
         return $this->connection;
     }
 }
