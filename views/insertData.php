6d5fba1555407845c4dd9929db83452d092d3960
suvaryanserhiy <suvaryanserhiy.ua@gmail.com>
2023-11-16 14:45:09 +0100

    added some comment to an insertData.php

--- views/insertData.php
+++ views/insertData.php
@@ -73,10 +73,15 @@
 </html>
 <?php
 
+// imports
 require_once('../models/Database.php');
 require_once('../controllers/OperationsController.php');
 
-session_start();
+
+
+session_start(); //initiation of a session
+
+// establish a setting
 $settings = parse_ini_file('../settings.ini');
 $server = $settings['server'];
 $username = $settings['username'];
@@ -84,15 +89,17 @@ $password = $settings['password'];
 $dbName = $settings['database'];
 
 try {
-    $connection = ($database = new Database($server, $username, $password, $dbName))->getConnection();
+    $connection = ($database = new Database($server, $username, $password, $dbName))->getConnection(); //getting a connection to a server
 
+
+    // managing an informative error if no table exists in databse
     try {
         $sql = "SHOW TABLES";
         $result = $connection->query($sql);
         //validation form input data
-        if ($result->num_rows < 1) {
+        if ($result->num_rows < 1) { //if sql sequence returns a table without at least 1 row
             $_SESSION['insertDBnull'] = true;
-            header('Location:index.php');
+            header('Location:index.php'); //redirect to index.php
             exit();
         }
     } catch (Exception $e) {
@@ -103,6 +110,7 @@ try {
 }
 if ($_SERVER['REQUEST_METHOD'] === "POST") {
 
+    //validating an input
     try {
         $name = isset($_POST['name']) ? $_POST['name'] : false;
         $telefono = isset($_POST['telefono']) ? $_POST['telefono'] : false;
@@ -110,18 +118,18 @@ if ($_SERVER['REQUEST_METHOD'] === "POST") {
         $email = isset($_POST['email']) ? $_POST['email'] : false;
         $table = isset($_POST['table']) ? $_POST['table'] : false;
 
-    } catch (Exception $e) {
+    } catch (Exception $e) { //show custum errors
         echo 'Error validating form data -> ' . $e;
         exit('error al establecer los datos') . $connection->$e;
     }
 
     //end of validation
 
-    if ($name && $telefono && $direccion && $email && $table) {
+    if ($name && $telefono && $direccion && $email && $table) { // if all are not false
         try {
-            $tableController = new OperationsController($connection);
+            $tableController = new OperationsController($connection); //Object for managing a table
 
-            $tableController->insertData($table,$name,$telefono,$direccion,$email);
+            $tableController->insertData($table,$name,$telefono,$direccion,$email); // execure insertation funtion
 
         } catch (RuntimeException $e) {
             echo "Error al insertar datos ->";
